56e4103a6354adc981974d6d7ffb414d05e54824
JAKLI <grot787898@gmail.com>
2017-06-30 18:33:07 +0300

    remove comment block

--- MVCTask/MVCTask/Startup.cs
+++ MVCTask/MVCTask/Startup.cs
@@ -18,39 +18,4 @@ namespace MVCTask
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
-
-    /*static class  MyClass
-    {
-        public static IHttpRoute MapHttpRoute(this HttpRouteCollection routes, string name, string routeTemplate, object defaults, object constraints, HttpMessageHandler handler)
-        {
-            if (routes == null)
-                throw new ArgumentNullException(nameof(routes));
-            HttpRouteValueDictionary routeValueDictionary1 = new HttpRouteValueDictionary(defaults);
-            HttpRouteValueDictionary routeValueDictionary2 = new HttpRouteValueDictionary(constraints);
-            IHttpRoute route = routes.CreateRoute(routeTemplate, (IDictionary<string, object>)routeValueDictionary1, (IDictionary<string, object>)routeValueDictionary2, (IDictionary<string, object>)null, handler);
-            routes.Add(name, route);
-            return route;
-        }
-
-        public static Route MapRoute(this RouteCollection routes, string name, string url, object defaults, object constraints, string[] namespaces)
-        {
-            if (routes == null)
-                throw new ArgumentNullException("routes");
-            if (url == null)
-                throw new ArgumentNullException("url");
-            Route route = new Route(url, (IRouteHandler)new MvcRouteHandler())
-            {
-                Defaults = RouteCollectionExtensions.CreateRouteValueDictionaryUncached(defaults),
-                Constraints = RouteCollectionExtensions.CreateRouteValueDictionaryUncached(constraints),
-                DataTokens = new RouteValueDictionary()
-            };
-            ConstraintValidation.Validate(route);
-            if (namespaces != null && namespaces.Length > 0)
-                route.DataTokens["Namespaces"] = (object)namespaces;
-            routes.Add(name, (RouteBase)route);
-            
-            return route;
-            
-        }
-    }*/
 }
